dd0b253993d03e488d315268581693d1e801a4de
tankeng <33891282+tankeng@users.noreply.github.com>
2018-04-19 21:56:43 +0700

    Update connect.js

--- src/connect.js
+++ src/connect.js
@@ -33,10 +33,10 @@ class LineConnect extends LineAPI {
         let expireCH = moment("/Date("+xc+"-0700)/").toString();
         console.info("Token: "+icH.channelAccessToken);
         console.info("TokenExpire: "+expireCH+"\n");
-        console.info(`Botrun70 by Id socool290 team\n`);
+        console.info(`Botrun by: Issara Team\n`);
         console.info(`Please don't share this project\n`);
-        console.info(`=======BOT BY MIN======\n`);
-        console.info(`=======ID LINE socool290======\n`);
+        console.info(`======= BOT BY: ™ചচ✾ъπ່७✾ざণاعနัю❍ีざန ======\n`);
+        console.info(`======= ID LINE tk_freeman ======\n`);
         resolve();
       });
     });
